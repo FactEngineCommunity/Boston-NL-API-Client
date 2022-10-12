3592b16ee5975923d3dd176e83cef3726a585f5a
Viev <victormorgante@viev.com>
2022-10-12 20:00:39 +1000

    - Initial commit

---
+++ WindowsApp1/Forms/frmMain.vb
@@ -0,0 +1,38 @@
+﻿Imports System.ServiceModel
+
+Public Class frmMain
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Call Me.SetupForm()
+
+        'Dim lsServiceEndpointUri As String = "http://131.176.82.134:8080/WCFServices/DuplexService"
+
+
+        'Dim DuplexCallback As New DuplexCallback
+
+        'Dim instanceContext As New InstanceContext(DuplexCallback)
+
+        'Dim dualHttpBinding As New WSDualHttpBinding(WSDualHttpSecurityMode.None)
+        'dualHttpBinding.OpenTimeout = New TimeSpan(0, 0, 10)
+        'Dim endpointAddress As New EndpointAddress(lsServiceEndpointUri)
+        'Dim lrDuplexServiceClient As New DuplexServiceClient(instanceContext, dualHttpBinding, endpointAddress)
+        'lrDuplexServiceClient.Open()
+        'lrDuplexServiceClient.Connect()
+
+        'Dim lrBroadcast As New Viev.FBM.Interface.Broadcast
+        'lrBroadcast.Model = New Viev.FBM.Interface.Model
+        'lrBroadcast.Model.Name = "Test-Model"
+
+        'lrDuplexServiceClient.SendBroadcast(Viev.FBM.Interface.publicConstantspcenumBroadcastType.ModelGetModelIdByModelName, lrBroadcast)
+
+        'Debugger.Break()
+
+    End Sub
+
+    Private Sub SetupForm()
+
+
+    End Sub
+
+End Class
---
+++ WindowsApp1/Proxy/tDuplexCallback.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading
+
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.ServiceModel
+
+<CallbackBehavior(UseSynchronizationContext:=False)>
+Friend Class DuplexCallback
+    Implements IDuplexServiceCallback
+
+
+    Private _syncContext As SynchronizationContext = AsyncOperationManager.SynchronizationContext
+
+    ''' <summary>
+    ''' See prDuplexServiceClient.HandleBroadcastReceived
+    ''' See also frmMain where: AddHandler DuplexCallback.BroadcastEventReceived, AddressOf prDuplexServiceClient.HandleBroadcastReceived
+    ''' </summary>
+    Public Event BroadcastEventReceived As EventHandler(Of Viev.FBM.Interface.Broadcast)
+
+
+    Public Sub ReceiveBroadcast(aiBroadcastType As Viev.FBM.Interface.publicConstantspcenumBroadcastType, ByRef arObject As Viev.FBM.Interface.Broadcast) Implements IDuplexServiceCallback.ReceiveBroadcast
+
+        Dim lrBroadcast As Viev.FBM.Interface.Broadcast = arObject
+        '20220327-VM-Was _syncContext.Post(New SendOrPostCallback(AddressOf OnBroadcastEvent), lrBroadcast)
+        'To send data back to client, must use Send.
+        _syncContext.Send(New SendOrPostCallback(AddressOf OnBroadcastEvent), lrBroadcast)
+        arObject.Model.Namespace = lrBroadcast.Model.Namespace
+
+    End Sub
+
+    Private Sub OnBroadcastEvent(ByVal State As Viev.FBM.Interface.Broadcast)
+
+        Dim e As Viev.FBM.Interface.Broadcast = TryCast(State, Viev.FBM.Interface.Broadcast)
+        RaiseEvent BroadcastEventReceived(Me, e)
+    End Sub
+
+End Class
